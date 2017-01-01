68109a813f138a7c7cfcf34b97123eba0cb1fff5
hitesh.baldaniya <hitesh.baldaniya@raweng.com>
2017-01-01 18:41:23 +0530

    PHP SDK Version 3.0.0 released

---
+++ lib/src/models/result.php
@@ -0,0 +1,36 @@
+<?php
+namespace Contentstack\Result;
+
+
+class Result {
+    private $object;
+
+    /*
+     * Result constructor
+     * Result wrapper over the plain result for the future
+     * */
+    public function __construct($result = '') {
+        $this->object = $result;
+    }
+
+    /*
+     * toJSON
+     * To convert result object to json
+     * @param
+     * @return json format of the result
+     * */
+    public function toJSON() {
+        return $this->object;
+    }
+
+    /*
+     * get
+     * Get the keys from the object
+     * @param
+     *      string|key - key whose corresponding value to be retrieved
+     * @return Value
+     * */
+    public function get($key) {
+        return ($key && is_string($key)) ? $this->object[$key] : NULL;
+    }
+}
