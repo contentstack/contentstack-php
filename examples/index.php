f3c162bc6ca4fa60acfd0e10902a16c7eebee0fd
hiteshbal91 <hiteshbal91@gmail.com>
2017-01-18 01:19:47 +0530

    Exception class added for the custom error handling API Reference doc regenrated Error Handling added in the SDK Error will be handled more promisingly
    No Test cases are not written as there are many possible cases of the erros from API.
    Yes it was successful report is already attached.

--- examples/index.php
+++ examples/index.php
@@ -13,15 +13,20 @@ use Contentstack\Contentstack;
 include_once "../lib/index.php";
 
 $stack = Contentstack::Stack('API_KEY', 'ACCESS_TOKEN', 'ENV_NAME');
+try {
+    $result = $stack->ContentType('content_type_uid')->Query()->toJSON()->includeCount()->find();
+    \Contentstack\Utility\debug($result);
 
-$result = $stack->ContentType('content_type_uid')->Query()->toJSON()->includeCount()->find();
-\Contentstack\Utility\debug($result);
+    $result = $stack->ContentType('content_type_uid')->Entry('blt12345678901')->fetch();
+    \Contentstack\Utility\debug($result);
 
-$result = $stack->ContentType('content_type_uid')->Entry('blt12345678901')->fetch();
-\Contentstack\Utility\debug($result);
+    $result = $stack->ContentType('content_type_uid')->Query()->toJSON()->findOne();
+    \Contentstack\Utility\debug($result);
 
-$result = $stack->ContentType('content_type_uid')->Query()->toJSON()->findOne();
-\Contentstack\Utility\debug($result);
-
-$result = $stack->getLastActivities();
-\Contentstack\Utility\debug($result);
+    $result = $stack->getLastActivities();
+    \Contentstack\Utility\debug($result);
+} catch(Exception $e) {
+    echo "Message : ".$e->getMessage(); // returns message -> API -> error_message
+    echo "Code : ".$e->getCode(); // returns number -> API -> error_code
+    echo "Errors : ".print_r($e->getErrors()); // returns array -> API -> errors
+}
