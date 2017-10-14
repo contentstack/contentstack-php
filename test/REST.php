189d062b48505d993e0fca2ef3d19eba3904c592
rohit.mishra <rohit.mishra@raweng.com>
2017-10-14 16:49:07 +0530

    upadate testcases

--- test/REST.php
+++ test/REST.php
@@ -113,7 +113,7 @@ class REST
      * */
     public function createUserSession()
     {
-        $user = $this->sendRequest('user-session', array('user' => array('email' => 'rohit.mishra@raweng.com', 'password' => '***REMOVED***')));
+        $user = $this->sendRequest('user-session', array('user' => array('email' => '<Email-id>', 'password' => '<password>')));
         if (isset($user['user'])) {
             $this->set('user', $user['user']);
             $this->headers['authtoken'] = (isset($user['user']['authtoken'])) ? $user['user']['authtoken'] : '';
