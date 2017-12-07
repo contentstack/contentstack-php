2c19824ddd4e5dc7697a8af4281767cccf40f6c3
Rohit Mishra <Rohit>
2017-12-07 18:14:46 +0530

    Added new method under entries and Assets class

--- examples/index.php
+++ examples/index.php
@@ -12,10 +12,10 @@ ini_set('display_errors', 'On');
 use Contentstack\Contentstack;
 include_once "../src/index.php";
 
-   $stack = Contentstack::Stack('***REMOVED***', '***REMOVED***', 'mobile');
+   $stack = Contentstack::Stack('<API-KEY>', '<ACCESS-TOKEN>', '<DEVELOPMENT>');
 
 try {
-       $result = $stack->ContentType('ctwithallfields')->Query()->addParam('include_dimensions', 'true')->toJSON()->find(); 
+       //$result = $stack->ContentType('ctwithallfields')->Query()->addParam('include_dimensions', 'true')->toJSON()->find(); 
      // $result = $stack->ContentType('ctwithallfields')->Query()->addParam('include_dimensions', true)->toJSON()->find();
       //$result = $stack->ContentType('ctwithallfields')->Query()->includeCount('include_dimensions', true)->toJSON()->find();
        //$result = $stack->ContentType('ctwithallfields')->Entry('blt8d1ab7600ba4c2b4')->addParam('include_dimensions', true)->toJSON()->fetch();
