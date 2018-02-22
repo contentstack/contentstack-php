4609541267cd0f3d669a6786812c0a064b9d8dd7
Rohit Mishra <Rohit>
2018-02-22 19:14:04 +0530

    Created a Testcase file on modular block feature

--- test/constants.php
+++ test/constants.php
@@ -5,6 +5,7 @@ define('RESULT_PATH', __DIR__.'/result.json');
 
 // how many entries we want to create
 define('ENTRY_COUNT', 10);
+define('MODULAR_ENTRY_COUNT', 5);
 define('REF_ENTRY_COUNT', 2);
 define('LIMIT_ENTRY_COUNT', 10);
 
@@ -12,3 +13,4 @@ define('LIMIT_ENTRY_COUNT', 10);
 // fpr query test params
 define('CT_REF', 'reference');
 define('CT_ContentType', 'ctwithallfields');
+define('CT_ModularContentType', 'test_multiple');
