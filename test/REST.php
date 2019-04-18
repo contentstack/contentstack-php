6a5c4193c3ebdb75a14e72f584578d3a706720c5
Rohit Mishra <rohit.mishra@raweng.com>
2019-04-18 19:26:58 +0530

    updated the testcases

--- test/REST.php
+++ test/REST.php
@@ -115,7 +115,8 @@ class REST
      * */
     public function createUserSession()
     {
-       $user = $this->sendRequest('user-session', array('user' => array('email' => 'Email-id', 'password' => 'password')));
+       $user = $this->sendRequest('user-session', array('user' => array('email' => '', 'password' => '')));
+       \Contentstack\Utility\debug(($user));
         if (isset($user['user'])) {
             $this->set('user', $user['user']);
             $this->headers['authtoken'] = (isset($user['user']['authtoken'])) ? $user['user']['authtoken'] : '';
