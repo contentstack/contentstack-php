39bf2f9c5c4d9781d94f643bf324b29201188eb5
Rohit Mishra <rohit.mishra@raweng.com>
2019-05-22 16:01:04 +0530

    Updated the README

--- src/index.php
+++ src/index.php
@@ -17,7 +17,7 @@ abstract class Contentstack {
      *      string|environment    : Environment whose content to be fetched.
      *  @return Stack
      * */
-    public static function Stack($api_key = '***REMOVED***', $access_token = '***REMOVED***', $environment = 'mobile') {
+    public static function Stack($api_key = '', $access_token = '', $environment = '') {
         return new Stack($api_key, $access_token, $environment);
     }
 }
