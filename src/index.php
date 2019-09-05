aee1d43dd09968a0a1c0b3e6eaabeb29bae6de4b
rohit mishra <rohit.mishra@contentstack.com>
2019-09-06 00:26:21 +0530

    Region Support

--- src/index.php
+++ src/index.php
@@ -4,6 +4,7 @@ namespace Contentstack;
 use Contentstack\Stack\Stack;
 
 require_once __DIR__ . '/lib/models/stack.php';
+require_once __DIR__."/contentstackregion.php";
 
 /*
  *  Contentstack abstract class to provide access to Stack Object
@@ -12,8 +13,8 @@ abstract class Contentstack {
     /*
      *  Static method for the Stack constructor
      *  @param
-     *      string|api_key        : Built.io Contentstack Stack API KEY.
-     *      string|access_token   : Built.io Contentstack Stack ACCESS TOKEN of respected Stack.
+     *      string|api_key        : Contentstack Stack API KEY.
+     *      string|access_token   : Contentstack Stack ACCESS TOKEN of respected Stack.
      *      string|environment    : Environment whose content to be fetched.
      *  @return Stack
      * */
