46beb50301edfb36df5379f168fc054824fc5ecc
rohit mishra <rohit.mishra@contentstack.com>
2019-09-06 00:26:21 +0530

    Region Support

--- src/lib/models/stack.php
+++ src/lib/models/stack.php
@@ -32,8 +32,8 @@ class Stack {
     public function __construct($api_*** = '', $access_token = '', $environment = '', $region = '') {
        
     
-        if($region && $region =="Europe") {
-            $this->host = Europe.'-'.HOST ;
+        if($region && $region =="EU") {
+            $this->host = EU.'-'.HOST;
         }
         $this->header = Utility\validateInput('stack', array('api_***' => $api_***, 'access_token' => $access_token, 'environment' => $environment, 'region' => $region));
         $this->environment = $this->header['environment'];
