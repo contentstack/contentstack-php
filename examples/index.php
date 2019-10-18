861239a927ba92ccd86d9daf1f4ef446ff03e948
rohit mishra <rohit.mishra@contentstack.com>
2019-10-18 15:24:32 +0530

    Support Snippet feature

--- examples/index.php
+++ examples/index.php
@@ -10,20 +10,21 @@
 ini_set('display_errors', 'On');
 use Contentstack\Contentstack;
 include_once "../src/index.php";
-   $stack = Contentstack::Stack('<API_KEY>', '<ACCESS_TOKEN>', '<ENVIRONMENT>');
+   $stack = Contentstack::Stack('', '', '');
 try {
     // \Contentstack\Utility\debug($stack);
-       //$result = $stack->ContentType('test')->Query()->IncludeReferenceContentTypeUID()->toJSON()->find(); 
-       //$result = $stack->ContentType('ctwithallfields')->Query()->includeCount()->toJSON()->find(); 
+      //  $result = $stack->getContentTypes('{"include_snippet_schema": "false"}'); 
+    //   $result = $stack->ContentType('java')->Query()->toJSON()->find(); 
 
       //$result = $stack->ContentType('ctwithallfields')->Query()->addParam('include_dimension', 'true')->toJSON()->find();
-     // $result = $stack->ContentType('ctwithallfields')->Query()->addParam('include_dimensions', true)->toJSON()->find();
+      // $result = $stack->ContentType('ctwithallfields')->Query()->addParam('include_dimensions', true)->toJSON()->find();
        //$result = $stack->ContentType('ctwithallfields')->Entry('blt8d1ab7600ba4c2b4')->addParam('include_dimensions', true)->toJSON()->fetch();
       // $result = $stack->Assets()->Query()->addParam('key', 'true')->toJSON()->find();
         //$result = $stack->Assets('blt9b5825dd804a9067')->addParam('include_dimension', 'true')->fetch();
-    //   \Contentstack\Utility\debug(($result[0]));
+       \Contentstack\Utility\debug(($result));
 
 } catch(Exception $e) {
+    \Contentstack\Utility\debug("sdcnjksdcjkdsbcjd");
     echo "Message : ".$e->getMessage(); // returns message -> API -> error_message
     echo "Code : ".$e->getCode(); // returns number -> API -> error_code
     echo "Errors : ".print_r($e->getErrors()); // returns array -> API -> errors
