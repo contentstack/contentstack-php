d876c1970708e4799decb54e3eb9024018e142e0
rohit mishra <rohit.mishra@contentstack.com>
2019-11-05 18:10:49 +0530

    made changes in global fields

--- examples/index.php
+++ examples/index.php
@@ -14,17 +14,16 @@ include_once "../src/index.php";
 try {
     // \Contentstack\Utility\debug($stack);
       //  $result = $stack->getContentTypes('{"include_snippet_schema": "false"}'); 
-    //   $result = $stack->ContentType('java')->Query()->toJSON()->find(); 
-
-      //$result = $stack->ContentType('ctwithallfields')->Query()->addParam('include_dimension', 'true')->toJSON()->find();
+      // $result = $stack->ContentType('first_ct')->Query()->toJSON()->find(); 
+     // $result = $stack->ContentType('a')->Fetch();
+     // $result = $stack->ContentType('a')->Query()->includeSchema()->toJSON()->find();
       // $result = $stack->ContentType('ctwithallfields')->Query()->addParam('include_dimensions', true)->toJSON()->find();
-       //$result = $stack->ContentType('ctwithallfields')->Entry('blt8d1ab7600ba4c2b4')->addParam('include_dimensions', true)->toJSON()->fetch();
+     //  $result = $stack->ContentType('a')->Entry('blta07130f8b344b260')->includeContentType()->toJSON()->fetch();
       // $result = $stack->Assets()->Query()->addParam('key', 'true')->toJSON()->find();
         //$result = $stack->Assets('blt9b5825dd804a9067')->addParam('include_dimension', 'true')->fetch();
-       \Contentstack\Utility\debug(($result));
+      // \Contentstack\Utility\debug(($result));
 
 } catch(Exception $e) {
-    \Contentstack\Utility\debug("sdcnjksdcjkdsbcjd");
     echo "Message : ".$e->getMessage(); // returns message -> API -> error_message
     echo "Code : ".$e->getCode(); // returns number -> API -> error_code
     echo "Errors : ".print_r($e->getErrors()); // returns array -> API -> errors
