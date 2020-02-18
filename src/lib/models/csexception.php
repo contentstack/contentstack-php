5e92b49b828ffe3912062bd82ed33da73b6fd84e
Uttam K Ukkoji <uttamukkoji@gmail.com>
2020-02-18 18:50:40 +0530

    Api reference generated

--- src/lib/models/csexception.php
+++ src/lib/models/csexception.php
@@ -12,7 +12,7 @@ class CSException extends \Exception {
 
     function __construct($error, $http_code = 412) {
         $error = json_decode($error, true);
-        $this->error_message = (isset($error['error_message'])) ? $error['error_message'] : "It seems Built.io Contentstack is behaving badly. Please contact support@contentstack.io.";
+        $this->error_message = (isset($error['error_message'])) ? $error['error_message'] : "It seems Contentstack is behaving badly. Please contact support@contentstack.io.";
         $this->error_code    = (isset($error['error_code'])) ? $error['error_code'] : null;
         $this->errors        = (isset($error['errors'])) ? $error['errors'] : array();
         $this->http_code     = $http_code;
