3c2b7695b0ea277afebf185b8f70c5ce7f98a624
Uttam K Ukkoji <uttamukkoji@gmail.com>
2020-02-27 14:54:47 +0530

    Asset class Query method PHP standards

--- src/lib/models/entry.php
+++ src/lib/models/entry.php
@@ -1,41 +1,67 @@
 <?php
-/*
- * Dependency loading
+/**
+ * An entry is the actual piece of content that you want to publish. 
+ * Entries can be created for one of the available content types.
+ *  
+ * PHP version 5
+ * 
+ * @category  PHP
+ * @package   Contentstack
+ * @author    Uttam K Ukkoji <uttamukkoji@gmail.com>
+ * @author    Rohit Mishra <rhtmishra4545@gmail.com>
+ * @copyright 2012-2020 Contentstack. All Rights Reserved
+ * @license   https://github.com/contentstack/contentstack-php/blob/master/LICENSE.txt MIT Licence
+ * @link      https://pear.php.net/package/contentstack
  * */
 
 namespace Contentstack\Stack\ContentType\Entry;
 
 use Contentstack\Stack\ContentType\BaseQuery\BaseQuery;
-use Contentstack\Utility;
+use Contentstack\Support\Utility;
 
 require_once __DIR__."/base_query.php";
-require_once __DIR__."/../utility.php";
-
-class Entry extends BaseQuery {
+require_once __DIR__."/../../Support/Utility.php";
+/**
+ * Entry
+ * 
+ * @category  PHP
+ * @package   Contentstack
+ * @author    Uttam K Ukkoji <uttamukkoji@gmail.com>
+ * @author    Rohit Mishra <rhtmishra4545@gmail.com>
+ * @copyright 2012-2020 Contentstack. All Rights Reserved
+ * @license   https://github.com/contentstack/contentstack-php/blob/master/LICENSE.txt MIT Licence
+ * @link      https://pear.php.net/package/contentstack
+ * */
+class Entry extends BaseQuery
+{
     var $operation;
     var $_query;
     var $entryUid;
     var $contentType = array();
 
-    /*
-     * Entry
+    /**
      * Entry Class to initalize your Entry
-     * @param
-     *      entry_uid: Entry to be fetched.
+     * 
+     * @param string $entryUid    - Entry to be fetched.
+     * @param string $contentType - contentType of Entry to be fetched.
      * */
-    public function __construct($entryUid = '', $contentType = '') {
+    public function __construct($entryUid = '', $contentType = '')
+    {
         $this->entryUid = $entryUid;
         parent::__construct($contentType, $this);
-        if(!\Contentstack\Utility\isEmpty($entryUid)) 
+        if (!Utility::isEmpty($entryUid)) {
             return $this;
+        }
     }
 
-    /*
-     * fetch
+    /**
      * Fetch the specified entry
+     * 
+     * @return Request
      * */
-    public function fetch() {
+    public function fetch()
+    {
         $this->operation = __FUNCTION__;
-        return \Contentstack\Utility\contentstackRequest($this);
+        return Utility::contentstackRequest($this);
     }
 }
