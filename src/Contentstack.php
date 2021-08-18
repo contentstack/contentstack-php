4b16d3ef57d29f4fa8de4bd4342cc4e7af9d288d
Uttam Krishna Ukkoji <uttamukkoji@gmail.com>
2021-08-18 10:54:53 +0530

    :tada: Live Preview feature support added

--- src/Contentstack.php
+++ src/Contentstack.php
@@ -46,7 +46,7 @@ abstract class Contentstack
     public static function Stack($api_key = '',
         $access_token = '',
         $environment = '',
-        $config = array('region'=> '')
+        $config = array('region'=> '', 'live_preview' => array('enable' => false))
     ) {
         return new Stack($api_key, $access_token, $environment, $config);
     }
